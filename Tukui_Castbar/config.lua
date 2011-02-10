d2ba4a02f075e6a85694e9b17f44b75867822888
Sweeper <nostradamus_4h@web.de>
2011-02-09 17:05:13 -0800

    width/heigth, yDistance from center screen

--- Tukui_Castbar/config.lua
+++ Tukui_Castbar/config.lua
@@ -3,4 +3,15 @@ local addon, ns = ...
 ns.config={
     ["separateplayer"] = true, -- separate player castbar
     ["separatetarget"] = true, -- separate target castbar
+	["bigcastbars"] = true, -- Bigger more visible castbars, reccomended for casters.
+	player = {
+		["yDistance"] = -200, -- Player castbar distance from the center of the screen.
+		["width"] = 350, -- Width of player castbar.
+		["height"] = 26, -- Height of player castbar.
+	},
+	target = { 
+		["yDistance"] = -150, -- Target castbar distance from the center of the screen.
+		["width"] = 250, -- Width of target castbar.
+		["height"] = 21, -- Height of target castbar.
+	}
 }
