a56a6e07928c1d5ac2112f7fceacda5af6147615
Sweeper <nostradamus_4h@web.de>
2011-02-16 08:59:41 -0800

    added possibility to disable ticks for channeled spells

--- Tukui_Castbar/config.lua
+++ Tukui_Castbar/config.lua
@@ -10,6 +10,7 @@ ns.config={
 		["height"] = 26, -- Height of player castbar.
 		["iconright"] = false, -- Display icon on right side of the bar. (not working yet)
 		["fontsize"] = 12, -- Fontsize for spellname and cast time.
+		["showticks"] = true, -- Display icon on right side of the bar. (not working yet)
 		["fontcolor"] = {0.84, 0.75, 0.65}, -- Fontcolor for spellname and cast time.
 		["castingcolor"] = {1.0, 0.49, 0, 0.5}, -- Color for normal cast.
 		["channelingcolor"] = {0.32, 0.3, 1, 0.5}, -- Color for channeled spells.
